588293a355ce3010d2adb0f9b3022fb64c4b2077
Lucas Detlor <lucasjd0112@gmail.com>
2025-04-24 11:35:10 -0700

    v6273584

--- problem1.py
+++ problem1.py
@@ -16,37 +16,75 @@ import tkinter as tk
 from tkinter import *
 import math
 
-def factor(e):
-    print('event happened')
-    print(f'details: {e}')
-
-def swap1(e):
-    print('event happened')
-    print(f'details: {e}')
-
-def swap2(e):
-    print('event happened')
-    print(f'details: {e}')
-
 win = tk.Tk()
 win.attributes("-topmost",True)
+press1 = 0
+press2 = 0
+
+def factor(event):
+    print('event happened')
+    print(f'details: {event}')
+    b = e[0].get()
+    c = e[1].get()
+    b = float(b)
+    c = float(c)
+    try:
+        x1 = ((-1*b + math.sqrt(b**2 - 4*c))/(2))
+        try:
+            x2 = ((-1*b - math.sqrt(b**2 - 4*c))/(2))
+            x1 = round(x1, 2)
+            x2 = round(x2, 2)
+            answer = (f'{x1} & {x2}')
+        except:
+            answer = round(x1, 2)
+    except:
+        x1 = ('dne')
+        try:
+            x2 = ((-1*b - math.sqrt(b**2 - 4*c))/(2))
+            answer = round(x2, 2)
+        except:
+            answer = ('No Real Roots')
+    e[2].delete(0, tk.END)
+    e[2].insert(0, answer)
+
+def swap1(event):
+    print('event happened')
+    print(f'details: {event}')
+    global press1
+    press1 += 1
+    if press1 % 2 != 0:
+        b[0].config(text='-')
+    else:
+        b[0].config(text='+')
+
+def swap2(event):
+    print('event happened')
+    print(f'details: {event}')
+    global press2
+    press2 += 1
+    if press2 % 2 != 0:
+        b[1]
+
+
 
 l = []
 l.append(tk.Label(win, text='Trinomial Factorer')) #title
 l.append(tk.Label(win, text='Input coefficients for b and c.')) #instructions
 l.append(tk.Label(win, text='x^2')) #ax^2
 l.append(tk.Label(win, text='x')) #bx
-l.append(tk.Label(win, text='Factors of x are:')) #answer
+l.append(tk.Label(win, text='Factor(s) of x are:')) #answer
 
 e = []
 e.append(tk.Entry(win, text='',width=2)) #bx
 e.append(tk.Entry(win, text='',width=2)) #c
-e.append(tk.Entry(win, text='')) #answer
+e.append(tk.Entry(win, text='',width=15)) #answer
 
 b = []
 b.append(tk.Button(win, text='+',height=0,width=0)) #ax^2 + bx
+b[0].bind('<Button-1>', swap1)
 b.append(tk.Button(win, text='+')) #bx + c
 b.append(tk.Button(win, text='FACTOR ME!')) #solve
+b[2].bind('<Button-1>', factor)
 
 
 l[0].place(x=50, y=0)
@@ -58,5 +96,7 @@ l[3].place(x=105,y=43)
 b[1].place(x=115,y=40)
 e[1].place(x=135,y=43)
 b[2].place(x=60, y=75)
+l[4].place(x=30, y=105)
+e[2].place(x=125, y=105)
 
 win.mainloop()
